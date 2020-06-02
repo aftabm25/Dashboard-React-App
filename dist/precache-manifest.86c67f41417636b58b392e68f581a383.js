50105899deffec073138f297b43d91f7c3d42b0e
aftabm <aftabmohideen25@gmail.com>
2020-06-02 21:30:36 +0530

    Deployed to Firebase

---
+++ dist/precache-manifest.86c67f41417636b58b392e68f581a383.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333783bf77adc9b3421ba9aff1c2bc27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0601bad329a607006ad3",
+    "url": "/static/css/main.9e3bb90e.chunk.css"
+  },
+  {
+    "revision": "ca2f5bf9d61f0698dc60",
+    "url": "/static/js/2.1aa8c1f1.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.1aa8c1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0601bad329a607006ad3",
+    "url": "/static/js/main.3bdb25b4.chunk.js"
+  },
+  {
+    "revision": "b44f635455487699ca01",
+    "url": "/static/js/runtime-main.0a7eddd2.js"
+  },
+  {
+    "revision": "b6ae49fea529ee5634acc8af10d98331",
+    "url": "/static/media/LexendTera-Regular.b6ae49fe.ttf"
+  }
+]);
